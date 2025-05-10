0199d2c0c1ec216d863394ca9fdbb379a709a19c
Yuchen Xu <xxb9075@gmail.com>
2025-05-10 05:40:11 -0700

    *auto calibration*: reimport and edit info.plist for ios package, now can install properly with both ios and ios-simulator build.

--- YOLOiOSApp/OpenCVBridge.mm
+++ YOLOiOSApp/OpenCVBridge.mm
@@ -1,16 +1,13 @@
-#import "OpenCVBridge.h"
+#import "./OpenCVBridge.h"
 
-#if defined(__arm64__)
 // Real device - use OpenCV
 #import <opencv2/core/core.hpp>
 #import <opencv2/imgproc/imgproc.hpp>
 #import <opencv2/opencv.hpp>
-#endif
 
 @implementation OpenCVBridge
 
 + (BOOL)isOpenCVWorking {
-#if defined(__arm64__)
     // Real device implementation - use OpenCV
     // Create a simple OpenCV matrix
     cv::Mat testMat = cv::Mat::eye(3, 3, CV_8UC1);
@@ -22,11 +19,6 @@
     NSLog(@"OpenCV Version: %s", CV_VERSION);
     
     return isValid;
-#else
-    // Simulator implementation - just return true and log
-    NSLog(@"Running on simulator - OpenCV test skipped");
-    return YES;
-#endif
 }
 
 @end 
